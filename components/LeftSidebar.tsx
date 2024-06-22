97bc8744f178f962727b888e370cd601cdd955d2
Furkan Karacan <furkan.karacan@adesso.com.tr>
2024-06-22 21:57:50 +0300

    Implementing left side bar

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+const LeftSidebar = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+          href="/"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Youcastr
+          </h1>
+        </Link>
+
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathName === route || pathName.startsWith(`${route}/`);
+
+          return (
+            <Link
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+              key={label}
+              href={route}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
