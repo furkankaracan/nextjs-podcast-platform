02284437345558df4c8bbf0bec553d8d856b8517
Furkan Karacan <furkan.karacan@adesso.com.tr>
2024-06-23 15:57:56 +0300

    Convex setup, Clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://discrete-boxer-21.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
